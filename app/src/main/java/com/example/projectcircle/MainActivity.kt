8d944e2dc2b6a57ae039e30316c0bc8fc63d49c5
sdavis.dev <sederrickdavisbup@gmail.com>
2026-08-28 01:51:14 -0500

    ProjectCircle - contained test circles in lists, added LazyColumn for padding off the edge, added state that Compose watches, and added join button that adds to count.

--- app/src/main/java/com/example/projectcircle/MainActivity.kt
+++ app/src/main/java/com/example/projectcircle/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,21 +48,13 @@ data class Circle(
 @Composable
 fun CircleScreen() {
     Column {
-        Text(
-            text = "Welcome to Circle!"
-        )
+        Text(text = "Welcome to Circle!")
 
-        Spacer(
-            modifier = Modifier.height(16.dp)
-        )
+        Spacer(modifier = Modifier.height(16.dp))
 
-        Text(
-            text = "Your communities, your people."
-        )
+        Text(text = "Your communities, your people.")
 
-        Spacer(
-            modifier = Modifier.height(16.dp)
-        )
+        Spacer(modifier = Modifier.height(16.dp))
 
         val gaming = Circle(
             name = "Gaming",
@@ -70,8 +70,27 @@ fun CircleScreen() {
             description = "For college friends"
         )
 
-        CircleCard(gaming)
-        CircleCard(collegeFriends)
+        val trips = Circle(
+            name = "Trips",
+            memberCount = 5,
+            isPrivate = false,
+            description = null
+        )
+
+        val circles = listOf(
+            gaming,
+            collegeFriends,
+            trips
+        )
+
+        LazyColumn(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(circles) { circle ->
+                CircleCard(circle)
+            }
+        }
     }
 }
 
@@ -81,8 +100,12 @@ fun CircleCard(circle: Circle) {
         Column (
             modifier = Modifier.padding(16.dp)
         ) {
+            var memberCount by remember {
+                mutableStateOf(circle.memberCount)
+            }
+
             Text(circle.name)
-            Text("${circle.memberCount} members")
+            Text("$memberCount members")
             Text(
                 if (circle.isPrivate) {
                     "Private"
@@ -90,6 +113,15 @@ fun CircleCard(circle: Circle) {
                     "Public"
                 }
             )
+            Text(text = circle.description ?: "No description")
+
+            Button(
+                onClick = {
+                    memberCount += 1
+                }
+            ) {
+                Text("Join Circle")
+            }
         }
     }
 }
